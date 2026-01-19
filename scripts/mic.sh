fc72ab3382b93fa4714eb34109237010e946b6bb
vinayak-jaybhaye <vinayakjaybhaye795@gmail.com>
2026-01-19 10:54:12 +0530

    fixed waybar mic icon

--- scripts/mic.sh
+++ scripts/mic.sh
@@ -4,7 +4,7 @@ SOURCE=$(pactl get-default-source)
 MUTED=$(pactl get-source-mute "$SOURCE" | awk '{print $2}')
 
 if [ "$MUTED" = "yes" ]; then
-	echo '{"text":"󰍮","tooltip":"Microphone muted","class":"disabled"}'
+	echo '{"text":"󰍭","tooltip":"Microphone muted","class":"disabled"}'
 else
 	echo '{"text":"󰍬","tooltip":"Microphone active","class":"enabled"}'
 fi
