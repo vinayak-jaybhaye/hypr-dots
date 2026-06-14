7f45d7a258052af90dee64a1a90c79c23ac714fb
Vinayak Jaybhaye <vinayakjaybhaye795@gmail.com>
2026-06-14 14:04:45 +0530

    style: update Waybar aesthetics with new background styling, icon tweaks, and margin adjustments

--- scripts/hyprsunset-status.sh
+++ scripts/hyprsunset-status.sh
@@ -6,7 +6,7 @@
 # Dependencies: pgrep
 
 if pgrep -x hyprsunset >/dev/null 2>&1; then
-	echo '{"text":"󰖔","tooltip":"Hyprsunset Enabled","class":"enabled"}'
+	echo '{"text":"󰖔 ","tooltip":"Hyprsunset Enabled","class":"enabled"}'
 else
-	echo '{"text":"󰖙","tooltip":"Hyprsunset Disabled","class":"disabled"}'
+	echo '{"text":" ","tooltip":"Hyprsunset Disabled","class":"disabled"}'
 fi
